e5bf1437acda957c305de82e0557b20afd851ce3
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-05-25 20:49:53 -0700

    feat: cleanup compile-map.py

--- dev/compile-map.py
+++ dev/compile-map.py
@@ -7,49 +7,54 @@ import os
 import shutil
 import timeit
 import json
+import string
+import sys
+
+configs = {
+	'fast': {
+		'vvis': '${bin}/vvis -game ${game} -fast ${bspfile}',
+		'vrad': '${bin}/vrad -game ${game} -noao -StaticPropLighting -threads $threads -hdr -fast ${bspfile}',
+		'vbsp': '${bin}/vbsp -game ${game} ${file}',
+		'vbsp2': '${bin}/vbsp2 -game ${game} ${file}',
+	},
+	'normal': {
+		'vvis': '${bin}/vvis -game ${game} ${bspfile}',
+		'vrad': '${bin}/vrad -game ${game} -noao -textureshadows -StaticPropLighting -threads $threads -hdr ${bspfile}',
+		'vbsp': '${bin}/vbsp -game ${game} ${file}',
+		'vbsp2': '${bin}/vbsp2 -game ${game} ${file}',
+	},
+	'final': {
+		'vvis': '${bin}/vvis -game ${game} ${bspfile}',
+		'vrad': '${bin}/vrad -game ${game} -noao -final -textureshadows -StaticPropLighting -threads $threads -hdr -StaticPropPolys ${bspfile}',
+		'vbsp': '${bin}/vbsp -game ${game} ${file}',
+		'vbsp2': '${bin}/vbsp2 -game ${game} ${file}',
+	}
+}
+
+profilers = {
+	'vtune': '$vtune -collect hotspots $exe',
+	'perf': 'perf record $exe'
+}
 
 argparser = argparse.ArgumentParser(description='Test harness for vrad, vvis, vbsp')
 argparser.add_argument('--profile-vrad', action='store_true')
 argparser.add_argument('--profile-vbsp', action='store_true')
 argparser.add_argument('--profile-vvis', action='store_true')
-argparser.add_argument('--profiler', type=str, choices=['vtune', 'perf'])
+argparser.add_argument('--profiler', type=str, choices=list(profilers.keys()))
 argparser.add_argument('--skip-vvis', action='store_true')
 argparser.add_argument('--vbsp2', action='store_true')
 argparser.add_argument('--skip-vbsp', action='store_true')
 argparser.add_argument('--skip-vrad', action='store_true')
 argparser.add_argument('--show-graph', action='store_true')
 argparser.add_argument('--threads', default=-1, type=int, help='Number of threads to use. Defaults to half of your systems core count')
-argparser.add_argument('--config', nargs='+', default=['fast'], help='Configs to use/compare')
+argparser.add_argument('--config', nargs='+', default=['fast'], choices=list(configs.keys()), help='Configs to use/compare')
 argparser.add_argument('--game', type=str, default='p2ce', choices=['p2ce', 'momentum'], help='Games to compile for')
 argparser.add_argument('--bench', action='store_true', help='Benchmark the compilers')
 argparser.add_argument('map', metavar='Map', type=str, nargs=1, help='Map to compile')
 args = argparser.parse_args()
 
-configs = {
-	'fast': {
-		'vvis': './bin/linux64/vvis -game $game -fast $bspfile',
-		'vrad': './bin/linux64/vrad -game $game -noao -StaticPropLighting -threads $threads -hdr -fast $bspfile',
-		'vbsp': './bin/linux64/vbsp -game $game $file',
-		'vbsp2': './bin/linux64/vbsp2 -game $game $file',
-	},
-	'normal': {
-		'vvis': './bin/linux64/vvis -game $game $bspfile',
-		'vrad': './bin/linux64/vrad -game $game -noao -textureshadows -StaticPropLighting -threads $threads -hdr $bspfile',
-		'vbsp': './bin/linux64/vbsp -game $game $file',
-		'vbsp2': './bin/linux64/vbsp2 -game $game $file',
-	},
-	'final': {
-		'vvis': './bin/linux64/vvis -game $game $bspfile',
-		'vrad': './bin/linux64/vrad -game $game -noao -final -textureshadows -StaticPropLighting -threads $threads -hdr -StaticPropPolys $bspfile',
-		'vbsp': './bin/linux64/vbsp -game $game $file',
-		'vbsp2': './bin/linux64/vbsp2 -game $game $file',
-	}
-}
-
-VTUNE_PATH="$vtune -collect hotspots $exe"
-PERF_PATH="perf record $exe"
 timers = []
-threads = multiprocessing.cpu_count() / 2
+threads = multiprocessing.cpu_count()
 
 # Dead simple timer class for recording elapsed times
 class Timer:
@@ -68,12 +73,26 @@ def check_result(result: subprocess.CompletedProcess):
 		print('\e[93mERROR: Compile FAILED!\n\e[0m')
 		exit(1)
 
+
+def get_bin_directory() -> str:
+	plat = 'linux64' if sys.platform.startswith('linux') else 'win64'
+	return os.path.abspath(os.path.dirname(__file__) + f'../../../bin/{plat}')
+
+
 def do_replacements(sourcemappath: str, bspfile: str, cmd: str) -> str:
 	global threads
-	return cmd.replace('$game', args.game).replace('$bspfile', bspfile).replace('$file', sourcemappath).replace('$threads', str(threads))
-	
+	template = string.Template(cmd)
+	return template.substitute({
+		'game': args.game,
+		'bspfile': bspfile,
+		'file': sourcemappath,
+		'threads': str(threads),
+		'bin': get_bin_directory()
+	})
+
+
 def inject_profiler():
-	p = VTUNE_PATH if args.profiler == 'vtune' else PERF_PATH
+	p = profilers[args.profiler]
 	for k in configs.keys():
 		if args.profile_vvis:
 			configs[k]['vvis'] = p.replace('$exe', configs[k]['vvis'])
@@ -83,7 +102,12 @@ def inject_profiler():
 			configs[k]['vbsp'] = p.replace('$exe', configs[k]['vbsp'])
 			configs[k]['vbsp2'] = p.replace('$exe', configs[k]['vbsp2'])
 
-def run_config(mapfile: str, config: str):
+
+def run_config(mapfile: str, config: str, dir: str):
+	# This needs to be absolute. The engine makes some nasty assumptions about where compilers are run from
+	if not os.path.isabs(mapfile):
+		mapfile = os.path.abspath(os.path.join(dir, mapfile))
+	
 	cfg = configs[config]
 	bspfile = mapfile.replace('.vmf', '.bsp')
 	isbsp = mapfile.endswith('.bsp')
@@ -119,12 +143,15 @@ def run_config(mapfile: str, config: str):
 
 	# Copy the file into game
 	try:
-		shutil.copy(bspfile, f'{args.game}/maps/{mapname}')
+		dest = f'{args.game}/maps/{mapname}'
+		shutil.copy(bspfile, dest)
+		print(f'Copied {bspfile} to {dest}')
 	except shutil.SameFileError:
 		pass # Eat it
 	
 	timers.append(timer)
-	
+
+
 def output_results(timers: list[Timer]):
 	outs = {}
 	outs['configs'] = configs
@@ -139,13 +166,13 @@ def output_results(timers: list[Timer]):
 		i += 1
 	with open(p, 'w') as fp:
 		json.dump(outs, fp)
-	
+
+
 def main():
-	global VTUNE_PATH
+	cwd = os.getcwd()
 	
-	# Expected to be in sdk_tools/dev
 	os.chdir(os.path.dirname(__file__) + '/../../')
-	
+
 	# Sanity checks..
 	if args.profiler == 'perf' and os.name == 'win32':
 		print('ERROR: perf is not supported on Windows!')
@@ -154,21 +181,23 @@ def main():
 	# Set vtune path
 	vtune_path = os.getenv('VTUNE')
 	if vtune_path is not None:
-		VTUNE_PATH = VTUNE_PATH.replace('$vtune', vtune_path)
+		profilers['vtune'] = profilers['vtune'].replace('$vtune', vtune_path)
 	elif os.name == 'win32' and args.profiler == 'vtune':
 		print('ERROR: VTUNE environment variable must point to vtune.exe!')
 		exit(1)
 	else:
-		VTUNE_PATH = VTUNE_PATH.replace('$vtune', '/opt/intel/oneapi/vtune/latest/bin64/vtune')
+		profilers['vtune'] = profilers['vtune'].replace('$vtune', '/opt/intel/oneapi/vtune/latest/bin64/vtune') # Should pretty much always be installed here
 	
 	# Now run!
-	inject_profiler()
+	if args.profiler is not None:
+		inject_profiler()
 	for c in args.config:
-		run_config(args.map[0], c)
+		run_config(args.map[0], c, cwd)
 	
 	# Post build results
 	if args.bench:
 		output_results(timers)
+
 
 if __name__ == '__main__':
 	main()
